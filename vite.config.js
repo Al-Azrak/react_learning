503f62490604cf737803e59accf847624c1c9a56
Al-Azrak <40920318+Al-Azrak@users.noreply.github.com>
2024-08-27 22:28:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "https://github.com/Al-Azrak/react_learning,
+    base: "https://github.com/Al-Azrak/react_learning/",
   plugins: [react()],
 })
